f7682d589f1fa74f7a62adc9969aa7e745d21095
Dev Kaliper <gxlthz@gmail.com>
2023-06-22 20:04:26 -0500

    rediseñando

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,125 +1,55 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import {
   Navbar,
-  MobileNav,
   Typography,
-  Button,
   IconButton,
+  Button,
+  Input,
 } from "@material-tailwind/react";
+import { BellIcon, Cog6ToothIcon } from "@heroicons/react/24/solid";
  
 export default function NavBar() {
-  const [openNav, setOpenNav] = useState(false);
- 
-  useEffect(() => {
-    window.addEventListener("resize", () => window.innerWidth >= 960 && setOpenNav(false));
-  }, []);
- 
-  const navList = (
-    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
-      >
-        <a href="#" className="flex items-center">
-          Pages
-        </a>
-      </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
-      >
-        <a href="#" className="flex items-center">
-          Account
-        </a>
-      </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
-      >
-        <a href="#" className="flex items-center">
-          Blocks
-        </a>
-      </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
-      >
-        <a href="#" className="flex items-center">
-          Docs
-        </a>
-      </Typography>
-    </ul>
-  );
- 
   return (
-    <Navbar className="mx-auto my-3 max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4">
-      <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
-        <Link
-          
-          to="/"
-          className="mr-4 cursor-pointer py-1.5 font-medium"
+    <Navbar
+      variant="gradient"
+      color="blue-gray"
+      className="mx-auto max-w-screen-xl from-blue-gray-900 to-blue-gray-800 px-4 py-3 my-4"
+    >
+      <div className="flex flex-wrap items-center justify-between gap-y-4 text-white">
+        <Typography
+          as="a"
+          href="#"
+          variant="h6"
+          className="mr-4 ml-2 cursor-pointer py-1.5"
         >
-          Arboles UTP 
-        </Link>
-        <div className="hidden lg:block">{navList}</div>
-        <Button variant="gradient" size="sm" className="hidden lg:inline-block">
-          <Link to="/about"> ¿quienes somos? </Link>
-        </Button>
-        <IconButton
-          variant="text"
-          className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-          ripple={false}
-          onClick={() => setOpenNav(!openNav)}
-        >
-          {openNav ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              className="h-6 w-6"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          )}
-        </IconButton>
-      </div>
-      <MobileNav open={openNav}>
-        <div className="container mx-auto">
-          {navList}
-          <Button variant="gradient" size="sm" fullWidth className="mb-2">
-            <span>Buy Now</span>
+          Arboles UTP
+        </Typography>
+        <div className="ml-auto flex gap-1 md:mr-4">
+          <IconButton variant="text" color="white">
+            <Cog6ToothIcon className="h-4 w-4" />
+          </IconButton>
+          <IconButton variant="text" color="white">
+            <BellIcon className="h-4 w-4" />
+          </IconButton>
+        </div>
+        <div className="relative flex w-full gap-2 md:w-max">
+          <Input
+            type="search"
+            color="white"
+            label="Buscar árbol"
+            className="pr-20"
+            containerProps={{
+              className: "min-w-[288px]",
+            }}
+          />
+          <Button
+            size="sm"
+            color="white"
+            className="!absolute right-1 top-1 rounded"
+          >
+            Buscar
           </Button>
         </div>
-      </MobileNav>
+      </div>
     </Navbar>
   );
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,34 +1,32 @@
 const Welcome = () => {
-    
-    return (<>
+  return (
+    <>
+      <div className="overflow-hidden ">
+        <div className="flex h-screen w-screen flex-col items-center justify-start overflow-hidden px-2">
+          <div className="flex flex-col items-center justify-start">
+            {/* MENSAJE DE BIENVENIDA */}
+            <h1 className="my-2 text-2xl font-bold text-white md:text-4xl xl:text-6xl">
+              ¡ Bienvenidos a Arboles UTP !
+            </h1>
+            <h2
+              className=" md:text-2 xl
+                text-center text-base font-bold italic text-gray-600 xl:text-2xl">
+              Un espacio dedicado a los arboles de nuestra universidad
+            </h2>
+          </div>
 
-    <div className="px-8 min-h-full min-w-full ">
-        <div className="flex flex-col gap-3 justify-center items-center md:flex-row md:justify-between w-full h-auto  p-3">
-        <h1 className="text-[#1F383E] font-semibold  text-6xl md:flex-grow ">¡Bienvenidos a Arboles UTP!</h1>
-        <input type="text" placeholder="Buscar..." className="h-8 w-3/4 md:w-1/2 p-2 rounded-xl shadow-xl  drop-shadow-sm outline-none" />
-
-        </div>
-
-        <div className="my-5 flex gap-5 w-full h-full " >
+          {/* SVG CON IMAGEN (YA NO HAY SVG) */}
+          <div id="SVG-container">
+            {/* TODO. 
+             HACER RESPONSIVE LA IMAGEN
+            */}
+            <img className="absolute object-cover top-0 left-0 w-full h-full" src="https://i.ibb.co/z4c8nmC/ft-Entrada.png" alt="" />
             
-                <img src="https://i.ibb.co/z4c8nmC/ft-Entrada.png" alt="imagen de entrada" className="rounded-3xl w-1/2 h-1/4"/>
-            
-
-            <div id="parrafo-de-entrada " className="py-4 flex flex-col justify-around w-1/2 max-h-1/2 items-center relative"
-            >
-                <h3 className="text-[#1F383E] font-semibold  text-3xl ">Una página web dedicada especialmente a los arboles de nuestra Universidad</h3>
-
-                <p className="text-[#1F383E] font-bold  text-lg text-start italic">Lorem ipsum dolo Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                <div>
-                    <span className="text-white text-2xl text-start"> Junio 34 </span>
-                </div>
-            </div>
+          </div>
         </div>
-    </div>
-    
-    
-    
-    </>)
-}
+      </div>
+    </>
+  );
+};
 
 export default Welcome;
